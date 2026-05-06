a157946bb9224ec5f0accc94c53f4fd744d10cb5
Jim Klick <jklick@ivytech.edu>
2026-05-06 15:57:36 -0400

    upgrade confirmations

--- cannon_upgrade.gd
+++ cannon_upgrade.gd
@@ -1,12 +1,23 @@
 extends Area2D
 
+var player = null
 
 func _on_ready():
 	$AnimationPlayer.play("hover")
 	
 func _on_body_entered(body):
 	if body.name == "player":
+		player = body
 		body.cannon_level = 3
 		GameState.cannon_level = body.cannon_level
-		body.upgrade_sound()
-		queue_free()
+		$CollisionShape2D.disabled = true 
+		hide()
+		$upgradeDialog.popup_centered()
+		get_tree().paused = true
+
+func _on_upgrade_dialog_confirmed():
+	get_tree().paused = false
+	if player:
+		player.upgrade_sound()
+	
+	queue_free()
--- rocket_upgrade.gd
+++ rocket_upgrade.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+var player = null
 
 func _on_ready():
 	$AnimationPlayer.play("hover")
@@ -8,7 +8,18 @@ func _on_ready():
 	
 func _on_body_entered(body):
 	if body.name == "player":
+		player = body
 		body.max_jumps += 1
 		GameState.max_jumps = body.max_jumps
-		body.upgrade_sound()
-		queue_free()
+		$CollisionShape2D.disabled = true 
+		hide()
+		$upgradeDialog.popup_centered()
+		get_tree().paused = true
+
+
+func _on_upgrade_dialog_confirmed() -> void:
+	get_tree().paused = false
+	if player:
+		player.upgrade_sound()
+	
+	queue_free()
